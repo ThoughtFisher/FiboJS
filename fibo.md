14d1dda41676db88927af982b2f59a52997d3874
jaime <jaime.molina@mattersupply.co>
2020-02-13 21:43:10 -0500

    fibo memo

--- fibo.md
+++ fibo.md
@@ -2,8 +2,12 @@
 
 `F(n) = F(n-1) + F(n-2)`
 
-1. Recurción.
+## 1. Recurción.
 
-## ¿De qué trata?
+#### ¿De qué trata?
 
-De llamarse a si mismo.
+##### De llamarse a si mismo.
+
+## 2. Memoization.
+
+#### Tecnica para optimzar el tiempo de respuesta de solución mediante el almacenamiento de resultados previos
--- fibonaci.js
+++ fibonaci.js
@@ -6,8 +6,17 @@
 // ej: F(1) => 1
 //     F(5) => 5
 
-function fibonacci(num) {
+function fibonacciR(num) {
   if (num <= 1) return 1
 
   return fibonacci(num - 1) + fibonacci(num - 2)
+}
+
+function fibonacciMemo(num, memo) {
+  memo = memo || {}
+
+  if (memo[num]) return memo[num]
+  if (num <= 1) return 1
+
+  return (memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo))
 }
